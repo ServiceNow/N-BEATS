c6fc187cd680cbf8fcf34fbb2d80d1473dd3e41e
Dmitri Carpov <dmitri.carpov@elementai.com>
2019-06-10 15:07:33 -0400

    wip: migrate to a cleaner implementation

--- m4_borgy.py
+++ m4_borgy.py
@@ -1,2 +1,13 @@
+import getpass
 import os
 
+borgy_args = [
+    "--image=images.borgy.elementai.lan/nbeats/base:%s" % getpass.getuser(),
+    "-v", "%s:/project" % os.path.basename(os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))),
+    "-w", "/project/sources",
+    "-e", "PYTHONPATH=/project/sources",
+    "--cpu=1",
+    "--gpu=1",
+    "--mem=16",
+    "--restartable"
+]
