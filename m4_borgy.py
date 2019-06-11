af51a0bc2106e62318fbc1b512d2ba2ec22a0acc
Dmitri Carpov <dmitri.carpov@elementai.com>
2019-06-11 11:02:50 -0400

    wip: migrate to a cleaner implementation

--- m4_borgy.py
+++ m4_borgy.py
@@ -3,9 +3,16 @@ import getpass
 import os
 from subprocess import Popen
 
+from m4.settings import M4_EXPERIMENTS_DIR
+
 image_name = 'images.borgy.elementai.net/nbeats:%s' % getpass.getuser()
+
 source_directory = os.path.dirname(os.path.realpath(__file__))
 project_directory = os.path.dirname(source_directory)
+
+container_project_path = os.path.join(os.sep, 'project')
+container_source_path = os.path.join(container_project_path, 'source')
+log_file_name = 'experiment.log'
 
 
 def build_and_push():
@@ -15,24 +22,24 @@ def build_and_push():
 
 def submit_to_borgy(experiments_dir_name):
     borgy_args = [
-        "--image=%s" % image_name,
-        "-v", "%s:/project" % project_directory,
-        "-w", "/project/sources",
-        "-e", "PYTHONPATH=/project/sources",
-        "--cpu=1",
-        "--gpu=1",
-        "--mem=16",
-        "--restartable"
+        '--image=%s' % image_name,
+        '-v', '%s:%s' % (project_directory, container_project_path),
+        '-w', container_source_path,
+        '-e', 'PYTHONPATH=%s' % container_source_path,
+        '--cpu=1',
+        '--gpu=1',
+        '--mem=16',
+        '--restartable'
     ]
-
     experiments_path = os.path.join(project_directory, 'experiments', experiments_dir_name)
 
     for i, experiment in enumerate(os.listdir(experiments_path)):
-        experiment_path = os.path.join(experiments_path, experiment)
         print('Experiment %d: %s' % (i, experiment))
-        command = 'python m4_main.py train --name %s/%s >> %s/experiment.log 2>&1' % (
-            experiments_dir_name, experiment, experiment_path)
-        print(' '.join(['borgy', 'submit', '--name=%s/%s' % (experiments_dir_name, experiment)] + borgy_args + ['--', 'bash', '-c',
+        command = 'python m4_main.py train --name %s/%s >> %s 2>&1' % (
+            experiments_dir_name, experiment,
+            os.path.join(M4_EXPERIMENTS_DIR, experiments_dir_name, experiment, log_file_name))
+        print(' '.join(
+            ['borgy', 'submit', '--name=%s/%s' % (experiments_dir_name, experiment)] + borgy_args + ['--', 'bash', '-c',
                                                                                                      command]))
         Popen(
             ['borgy', 'submit', '--name=%s/%s' % (experiments_dir_name, experiment)] + borgy_args + ['--', 'bash', '-c',
