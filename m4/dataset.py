fix masking

m4/dataset.py
@@ -37,6 +37,7 @@ class M4DatasetSplit(Enum):
 class M4Batch(NamedTuple):
     horizon: int
     inputs: np.ndarray
+    input_masks: np.ndarray
     targets: np.ndarray
     target_mask: np.ndarray
     masep: np.ndarray
@@ -81,6 +82,7 @@ class M4Dataset:
         indices_subset = list(set(indices_filter).intersection(set(timeseries_with_same_horizon)))
 
         batch_input = np.zeros(shape=(batch_size, M4_INPUT_MAXSIZE), dtype=np.float32)
+        batch_input_masks = np.zeros(shape=(batch_size, M4_INPUT_MAXSIZE), dtype=np.float32)
         batch_target = np.zeros(shape=(batch_size, sampled_horizon), dtype=np.float32)
         batch_target_mask = np.zeros(shape=(batch_size, sampled_horizon), dtype=np.float32)
         history_limit = M4_SAMPLING_WINDOW_LIMIT[sampled_horizon]
@@ -92,11 +94,13 @@ class M4Dataset:
             ts_input = np.flip(ts[max(0, sampled_point - M4_INPUT_MAXSIZE):sampled_point])
             ts_target = ts[sampled_point:min(sampled_point + sampled_horizon, len(ts))]
             batch_input[i, :len(ts_input)] = ts_input
+            batch_input_masks[i, :len(ts_index)] = 1.0
             batch_target[i, :len(ts_target)] = ts_target
             batch_target_mask[i, :len(ts_target)] = 1.0
 
         return M4Batch(horizon=sampled_horizon,
                        inputs=batch_input,
+                       input_masks=batch_input_masks,
                        targets=batch_target,
                        target_mask=batch_target_mask,
                        masep=self.masep[sampled_indices])
@@ -109,10 +113,12 @@ class M4Dataset:
             batch_indices = indices_for_horizon[i * batch_size: (i + 1) * batch_size]
             batch_timeseries = [self.data[j] for j in batch_indices]
             inputs = np.zeros(shape=(len(batch_timeseries), M4_INPUT_MAXSIZE), dtype=np.float32)
+            input_masks = np.zeros(shape=(len(batch_timeseries), M4_INPUT_MAXSIZE), dtype=np.float32)
             for ts_index, ts in enumerate(batch_timeseries):
                 ts_last_window = np.flip(ts[max(0, len(ts) - M4_INPUT_MAXSIZE):])
                 inputs[ts_index, :len(ts_last_window)] = ts_last_window
-            yield inputs
+                input_masks[ts_index, :len(ts_last_window)] = 1.0
+            yield inputs, input_masks
 
     def __get_cached_dataset(self, cache_file_path: str) -> np.ndarray:
         if not os.path.isfile(cache_file_path):

m4/experiment.py
@@ -7,7 +7,7 @@ import numpy as np
 
 params_file_name = 'params.json'
 ts_indices_file_name = 'ts_ids_subset.npz'
-input_mask_file_name = 'input_mask.npz'
+input_dropout_file_name = 'input_dropout.npz'
 
 
 class ExperimentParameters(NamedTuple):
@@ -60,18 +60,18 @@ class ExperimentParameters(NamedTuple):
 class M4Experiment(NamedTuple):
     parameters: ExperimentParameters
     timeseries_indices: np.ndarray
-    input_mask: np.ndarray
+    input_dropout: np.ndarray
 
     def persist(self, experiment_dir_path: str) -> None:
         Path(experiment_dir_path).mkdir(parents=True, exist_ok=False)
         self.parameters.persist(os.path.join(experiment_dir_path, params_file_name))
         self.timeseries_indices.dump(os.path.join(experiment_dir_path, ts_indices_file_name))
-        self.input_mask.dump(os.path.join(experiment_dir_path, input_mask_file_name))
+        self.input_dropout.dump(os.path.join(experiment_dir_path, input_dropout_file_name))
 
     @staticmethod
     def load(experiment_dir_path: str) -> 'M4Experiment':
         return M4Experiment(parameters=ExperimentParameters.load(os.path.join(experiment_dir_path, params_file_name)),
                             timeseries_indices=np.load(os.path.join(experiment_dir_path, ts_indices_file_name),
                                                        allow_pickle=True),
-                            input_mask=np.load(os.path.join(experiment_dir_path, input_mask_file_name),
-                                               allow_pickle=True))
+                            input_dropout=np.load(os.path.join(experiment_dir_path, input_dropout_file_name),
+                                                  allow_pickle=True))

m4/model.py
@@ -8,21 +8,12 @@ from tensorflow.contrib import slim
 from m4.dataset import M4Dataset, M4DatasetSplit
 from m4.experiment import M4Experiment
 from m4.settings import M4_INPUT_MAXSIZE, M4_PREDICTION_FILE_NAME
-from m4.utils import summary_log, ScaledVarianceRandomNormal
+from m4.utils import summary_log
 from nbeats import NBeats, NBeatsStack, NBeatsBlock, NBeatsHarmonicsBlock, NBeatsPolynomialBlock
 
 
-def model_graph(input_placeholder, experiment: M4Experiment, horizons: Iterable) -> Dict:
+def model_graph(input_placeholder, input_mask_placeholder, experiment: M4Experiment, horizons: Iterable) -> Dict:
     models = {}
-    # model_scope = slim.arg_scope(
-    #     [slim.],
-    #     activation_fn=None,
-    #     normalizer_fn=None,
-    #     trainable=is_training,
-    #     weights_regularizer=None,
-    #     weights_initializer=ScaledVarianceRandomNormal(factor=0.1),
-    # )
-    # with model_scope:
     with tf.variable_scope('M4-model', reuse=tf.AUTO_REUSE):
         for horizon in horizons:
             with tf.variable_scope(f'horizon_{horizon}', reuse=tf.AUTO_REUSE):
@@ -31,14 +22,11 @@ def model_graph(input_placeholder, experiment: M4Experiment, horizons: Iterable)
                 apply_input_delevel = horizon == 13 or horizon == 48
 
                 # delevel input of Hourly and Weekly
-                # TODO: fix UGLY mask issue
                 input_level = model_input[:, :1]
                 if apply_input_delevel:
-                    mask = tf.not_equal(model_input, tf.zeros_like(model_input))
-                    model_input = model_input - input_level - 0.01
-                    model_input = tf.multiply(model_input, tf.cast(mask, model_input.dtype))
+                    model_input = model_input - input_level
 
-                model_input = tf.multiply(model_input, experiment.input_mask[None, :input_size],
+                model_input = tf.multiply(model_input, experiment.input_dropout[None, :input_size],
                                           name='model_input')
 
                 if experiment.parameters.model_type == 'generic':
@@ -75,9 +63,9 @@ def model_graph(input_placeholder, experiment: M4Experiment, horizons: Iterable)
                     raise Exception(f'Unknown model type {experiment.parameters.model_type}')
 
                 if apply_input_delevel:
-                    models[horizon] = model.build(model_input) + input_level
+                    models[horizon] = model.build(model_input, input_mask=input_mask_placeholder) + input_level
                 else:
-                    models[horizon] = model.build(model_input)
+                    models[horizon] = model.build(model_input, input_mask=input_mask_placeholder)
     return models
 
 
@@ -105,6 +93,9 @@ def train(experiment_path: str):
             inputs = tf.placeholder(shape=(batch_size, M4_INPUT_MAXSIZE),
                                     name='inputs',
                                     dtype=tf.float32)
+            input_masks = tf.placeholder(shape=(batch_size, M4_INPUT_MAXSIZE),
+                                         name='input_masks',
+                                         dtype=tf.float32)
             masep = tf.placeholder(shape=(batch_size,),
                                    name='masep',
                                    dtype=tf.float32)
@@ -184,8 +175,9 @@ def train(experiment_path: str):
                     batch = training_set.sample_batch(batch_size=batch_size,
                                                       indices_filter=experiment.timeseries_indices)
                     feed_dict = {
-                        targets[batch.horizon]: batch.targets,
                         inputs: batch.inputs,
+                        input_masks: batch.input_masks,
+                        targets[batch.horizon]: batch.targets,
                         target_masks[batch.horizon]: batch.target_mask,
                         masep: batch.masep
                     }
@@ -195,7 +187,6 @@ def train(experiment_path: str):
                     if step % experiment.parameters.training_checkpoint_interval == 0:
                         train_log_writer(step, **train_log_results)
                         print(f'step {step}, loss: {batch_loss}', flush=True)
-                        # TODO: figure out why logging does not work
                         summary_str = sess.run(summary, feed_dict=feed_dict)
                         summary_writer.add_summary(summary_str, step)
                         summary_writer.flush()
@@ -211,6 +202,9 @@ def predict(experiment_path: str):
             inputs = tf.placeholder(shape=(None, M4_INPUT_MAXSIZE),
                                     name='inputs',
                                     dtype=tf.float32)
+            input_masks = tf.placeholder(shape=(None, M4_INPUT_MAXSIZE),
+                                         name='input_masks',
+                                         dtype=tf.float32)
         models = model_graph(inputs, experiment, training_set.info.horizons)
         config = tf.ConfigProto(allow_soft_placement=True)
         config.gpu_options.allow_growth = True
@@ -222,8 +216,8 @@ def predict(experiment_path: str):
 
         forecasts = []
         for horizon in training_set.info.horizons:
-            for batch in training_set.sequential_input_batches(1000, horizon):
-                forecasts.extend(session.run(models[horizon], feed_dict={inputs: batch}))
+            for batch, batch_mask in training_set.sequential_input_batches(1000, horizon):
+                forecasts.extend(session.run(models[horizon], feed_dict={inputs: batch, input_masks: batch_mask}))
 
         forecasts_df = pd.DataFrame(forecasts)
         forecasts_df.columns = [f'F{i}' for i in range(1, len(forecasts_df.columns) + 1)]

nbeats.py
@@ -151,18 +151,17 @@ class NBeatsStack:
     def __init__(self, blocks: List[NBeatsBlock]):
         self.blocks = blocks
 
-    def build(self, inputs):
+    def build(self, inputs, input_mask=None):
         with tf.variable_scope('nbeats-blocks', reuse=tf.AUTO_REUSE):
             residuals = inputs
             stack_forecast = []
             for block in self.blocks:
-                # TODO: fix 0-like mask, it works for M4 but is not generally applicable.
-                mask = tf.not_equal(residuals, tf.zeros_like(residuals))
                 backcast, forecast = block.build(residuals)
                 residuals = residuals - backcast
                 stack_forecast.append(forecast)
 
-                residuals = tf.multiply(residuals, tf.cast(mask, residuals.dtype))
+                if input_mask is not None:
+                    residuals = tf.multiply(residuals, tf.cast(input_mask, residuals.dtype))
         return residuals, tf.add_n(stack_forecast) if len(stack_forecast) > 0 else 0.0
 
 
@@ -171,11 +170,11 @@ class NBeats:
                  stacks: List[NBeatsStack]):
         self.stacks = stacks
 
-    def build(self, inputs):
+    def build(self, inputs, input_mask=None):
         residuals = inputs
         stacks_forecast = 0.0
         for i, stack in enumerate(self.stacks):
             with tf.variable_scope(f'nbeats-stack-{i}', reuse=tf.AUTO_REUSE):
-                residuals, forecast = stack.build(residuals)
+                residuals, forecast = stack.build(residuals, input_mask)
                 stacks_forecast = stacks_forecast + forecast
         return inputs[:, :1] + stacks_forecast