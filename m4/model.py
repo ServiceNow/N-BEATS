fix masking

m4/model.py
@@ -19,16 +19,15 @@ def model_graph(input_placeholder, input_mask_placeholder, experiment: M4Experim
             with tf.variable_scope(f'horizon_{horizon}', reuse=tf.AUTO_REUSE):
                 input_size = min(experiment.parameters.input_size * horizon, M4_INPUT_MAXSIZE)
                 model_input = input_placeholder[:, :input_size]
-                input_mask = input_mask_placeholder[:, :input_size]
+                input_mask = tf.multiply(input_mask_placeholder[:, :input_size],
+                                         experiment.input_dropout[None, :input_size])
+                # TODO: show benefit on validation
                 apply_input_delevel = horizon == 13 or horizon == 48
 
                 # delevel input of Hourly and Weekly
                 input_level = model_input[:, :1]
                 if apply_input_delevel:
                     model_input = model_input - input_level
-
-                model_input = tf.multiply(model_input, experiment.input_dropout[None, :input_size],
-                                          name='model_input')
 
                 if experiment.parameters.model_type == 'generic':
                     model = NBeats([NBeatsStack([NBeatsBlock(input_size=input_size,
@@ -128,6 +127,7 @@ def train(experiment_path: str):
                                                                     predictions=forecast,
                                                                     weights=weights)
                 elif loss_name == 'SMAPE':
+                    # stop gradient on denominator for numerical stability.
                     weights = tf.stop_gradient(tf.div_no_nan(2.0 * target_mask, (target + tf.abs(forecast))))
                     losses[horizon] = tf.reduce_mean(tf.abs(target - forecast) * weights)
                 else:

nbeats.py
@@ -93,7 +93,7 @@ class NBeatsPolynomialBlock(NBeatsBlock):
             backcast += tf.pow(t_backcast, float(i)) * basis_output[:, self.polynomial_order + 1 + i, None]
         return backcast, forecast
 
-
+# TODO: rename
 class NBeatsHarmonicsBlock(NBeatsBlock):
     def __init__(self,
                  input_size: int,
@@ -131,7 +131,6 @@ class NBeatsHarmonicsBlock(NBeatsBlock):
         harmonics_sin = tf.matmul(harmonics_weights[:, num_basis_fns:2 * num_basis_fns], sin_template)
         forecast = harmonics_cos + harmonics_sin
         # Produce backcast seasonality synthesis, this signal will be used by the next analysis step
-        # TODO: clarify why /forecast_horizon and not /input_size
         t_backcast = -np.arange(self.input_size, dtype=np.float32)[:, None] / self.forecast_horizon
         cos_template_backcast = np.transpose(np.cos(2 * np.pi * t_backcast * freq))
         sin_template_backcast = np.transpose(np.sin(2 * np.pi * t_backcast * freq))
@@ -156,12 +155,12 @@ class NBeatsStack:
             residuals = inputs
             stack_forecast = []
             for block in self.blocks:
+                if input_mask is not None:
+                    residuals = tf.multiply(residuals, tf.cast(input_mask, residuals.dtype))
+
                 backcast, forecast = block.build(residuals)
                 residuals = residuals - backcast
                 stack_forecast.append(forecast)
-
-                if input_mask is not None:
-                    residuals = tf.multiply(residuals, tf.cast(input_mask, residuals.dtype))
         return residuals, tf.add_n(stack_forecast) if len(stack_forecast) > 0 else 0.0
 
 